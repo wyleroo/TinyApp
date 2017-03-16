54609add56d39995f338001d5bddd858d18f0b67
Wyatt Robinson <thewyatt@gmail.com>
2017-03-16 02:20:11 +0000

    Updated partial header; added login and logout functions

--- express_server.js
+++ express_server.js
@@ -1,9 +1,12 @@
-// Set global declarations
+// Import modules and assign to express
 const express = require("express");
 const app = express();
-app.set("view engine", "ejs");
+const cookieParser = require('cookie-parser');
 const bodyParser = require("body-parser");
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser())
+
 const PORT = process.env.PORT || 8080;
 
 // Sample database - could be replaced later with input fxn
@@ -22,52 +25,59 @@ function generateRandomString() {
   return output;
 };
 
+//Post login; redirect to index
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username);
+  console.log(req.cookies);
+  res.redirect("/urls");
+});
+
 // Entry field for URL to shorten
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// URLs index page
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase, username: req.cookies['username'] };
+  res.render("urls_index", templateVars);
+});
+
+// List of long and short URL based on short URL as id
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id, link: urlDatabase[req.params.id], username: req.cookies['username'] };
+  res.render("urls_show", templateVars);
+});
+
 //Post request to handle new urls
 app.post("/urls", (req, res) => {
   let randomShorty = generateRandomString();
-  let newLongURL = req.body.longURL;
-  urlDatabase[randomShorty] = newLongURL;
-  console.log(newLongURL, randomShorty, urlDatabase);
-  res.redirect(newLongURL);
+  urlDatabase[randomShorty] = req.body.longURL;
+  res.redirect(req.body.longURL);
 });
 
+// Post request to delete cookies on logout
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect("/urls");
+});
+
+// Post request to delete database entries
 app.post("/urls/:id/delete", (req, res) => {
-  console.log(req.params.id);
   delete urlDatabase[req.params.id];
   res.redirect("/urls");
-})
+});
 
 // Redirecting from shortURL to longURL
 app.get("/u/:shortURL", (req, res) => {
   let redirectURL = urlDatabase[req.params.shortURL];
-  console.log(redirectURL);
   res.redirect(redirectURL);
 });
 
 //Redirect and update database from Update button
 app.post("/urls/:id/update", (req, res) => {
   urlDatabase[req.params.id] = req.body.newHash;
-
-  console.log(req.params.id,', newHash: ' ,req.body.newHash, urlDatabase);
   res.redirect("/urls");
-});
-
-// URLs index page
-app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
-// List of long and short URL based on short URL as id
-app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id,
-    link: urlDatabase[req.params.id] };
-  res.render("urls_show", templateVars);
 });
 
 // Listening for web requests
