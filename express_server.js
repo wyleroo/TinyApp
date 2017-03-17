45a240391d6d6a5b65bfbbd34e5a8a6bde8d7f34
Wyatt Robinson <thewyatt@gmail.com>
2017-03-17 22:52:47 +0000

    attempting a major rewrite of everything because my database structure is wrong

--- express_server.js
+++ express_server.js
@@ -11,8 +11,8 @@ const PORT = process.env.PORT || 8080;
 
 // Sample database - could be replaced later with input fxn
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {site: "http://www.lighthouselabs.ca", userPermission: "wiley"},
+  "9sm5xK": {site: "http://www.google.com", userPermission: "wiley"}
 };
 
 // Sample user database
@@ -48,10 +48,10 @@ function emailSearch(emailIn) {
 function passwordSearch(passwordIn) {
   for (id in users) {
     if (users[id].password == passwordIn){
-      return true
+      return true;
     }
   }
-  return false
+  return false;
 }
 
 // Function to return user id based on email match
@@ -61,7 +61,23 @@ function loginReturn(loginEmail) {
       return users[key].id;
     }
   }
-  return false
+  return false;
+}
+
+function urlsForUser(idRequest) {
+  let urlUser = {};
+  console.log('hello');
+  for (key in urlDatabase) {
+    console.log('there');
+    for (person in urlDatabase[key].userPermission) {
+      console.log('beautiful');
+      if (urlDatabase[key].userPermission[person] == idRequest) {
+        console.log(key);
+        urlUser[key] = urlDatabase.site;
+      }
+    }
+  }
+  return urlUser;
 }
 
 // Random string generator - to be set as randomShorty
@@ -109,26 +125,39 @@ app.post("/register", (req, res) => {
 
 // Entry field for URL to shorten
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  if (req.cookies.user_id){
+    res.render("urls_new");
+  } else {
+    res.redirect("login");
+  }
 });
 
 // URLs index page
 app.get("/urls", (req, res) => {
-  // Pass specified user object
-  let templateVars = { urls: urlDatabase, user_id: req.cookies.user_id };
+  let permitted = urlsForUser(req.cookies.user_id);
+  console.log('permitted', permitted);
+  let templateVars = { userPermisssion: permitted, urls: urlDatabase, user_id: req.cookies.user_id };
   res.render("urls_index", templateVars);
 });
 
 // List of long and short URL based on short URL as id
 app.get("/urls/:id", (req, res) => {
+  console.log('rpi', req.params);
   let templateVars = { shortURL: req.params.id, link: urlDatabase[req.params.id], user_id: req.cookies.user_id };
   res.render("urls_show", templateVars);
+});
+
+// Redirecting from shortURL to longURL
+app.get("/u/:shortURL", (req, res) => {
+  let redirectURL = urlDatabase[req.params.shortURL].site;
+  res.redirect(redirectURL);
 });
 
 //Post request to handle new urls
 app.post("/urls", (req, res) => {
   let randomShorty = generateRandomString();
-  urlDatabase[randomShorty] = req.body.longURL;
+  urlDatabase[randomShorty] = {randomShorty: {site: req.body.longURL, userPermission: req.cookies.user_id}};
+  console.log(urlDatabase);
   res.redirect(req.body.longURL);
 });
 
@@ -140,20 +169,25 @@ app.post("/logout", (req, res) => {
 
 // Post request to delete database entries
 app.post("/urls/:id/delete", (req, res) => {
-  delete urlDatabase[req.params.id];
-  res.redirect("/urls");
-});
-
-// Redirecting from shortURL to longURL
-app.get("/u/:shortURL", (req, res) => {
-  let redirectURL = urlDatabase[req.params.shortURL];
-  res.redirect(redirectURL);
+  if (req.cookies.user_id){
+      delete urlDatabase[req.params.id];
+        res.redirect("/urls");
+    } else {
+      res.status(401).send("No, don't");
+    }
 });
 
 //Redirect and update database from Update button
 app.post("/urls/:id/update", (req, res) => {
-  urlDatabase[req.params.id] = req.body.newHash;
-  res.redirect("/urls");
+  console.log('urlDatabase', urlDatabase);
+  if (req.cookies.user_id == urlDatabase[req.params.id].userPermission) {
+    urlDatabase[req.body.newHash] = urlDatabase[req.params.id];
+    urlDatabase[req.body.newHash].userPermission = req.cookies.user_id;
+    delete urlDatabase[req.params.id];
+    res.redirect("/urls");
+  } else {
+    res.status(401).send("Please no");
+  }
 });
 
 // Listening for web requests
